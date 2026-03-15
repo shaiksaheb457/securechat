b396069910a5b390946e7a161d611f63cc6cffca
shaiksaheb457 <shaiksaheb457@gmail.com>
2026-03-15 12:45:32 +0530

    fix port for railway

--- server.js
+++ server.js
@@ -74,6 +74,8 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(3000, () => {
-  console.log('SecureChat running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('SecureChat running on port ' + PORT);
 });
+
